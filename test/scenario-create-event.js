fa5672fa2f613363bba06943dd62879a09f1a4ff
Matthias Freßdorf <mfressdorf.mmt-m2014@fh-salzburg.ac.at>
2016-05-28 13:50:15 +0200

    Add failure scenarios; Upgrade dependencies

--- test/scenario-create-event.js
+++ test/scenario-create-event.js
@@ -30,9 +30,9 @@ describe('Run Specs for the non transactional system', () => {
   /**
    * Simulate a common Request to the Wrapper Api of the non transactional system
    * All 3 steps of the Operation should pass
-   * 1: Create Event
-   * 2: Create Job based on eventId
-   * 3: Update Event with jobId
+   * (SUCCESS) 1: Create Event
+   * (SUCCESS) 2: Create Job based on eventId
+   * (SUCCESS) 3: Update Event with jobId
    */
 
   describe('A Event POST request', () => {
@@ -84,16 +84,19 @@ describe('Run Specs for the non transactional system', () => {
    * Simulate a common Request to the Wrapper Api of the non transactional system
    * The Event API should throw an Error, immediately failing the Request
    * without creating an event, nor a job
+   * (FAIL) 1: Create Event
+   * (FAIL) 2: Create Job based on eventId
+   * (FAIL) 3: Update Event with jobId
    */
 
-  describe('A failed Event POST request', () => {
+  describe('A failed Event POST request (Event API fails on create)', () => {
     before('Clear up databases', (done) => clearUpDatabases(done));
     it('should fail', done => {
       supertest(nonTransactionalApp)
         .post('/events')
         .send({ publishingDate })
         .set('monkey_POST_event', 'none/500')
-        .expect(200, done)
+        .expect(500, done)
     });
 
     it('should not have created an Event in MongoDB', done => {
@@ -113,6 +116,93 @@ describe('Run Specs for the non transactional system', () => {
         .count()
         .then(res => {
           expect(res).to.be.equal(0);
+          done();
+        })
+        .catch(err => {
+          done(err);
+        });
+    });
+  });
+
+  /**
+   * Simulate a common Request to the Wrapper Api of the non transactional system
+   * The Job API should throw an Error, while an Event was already created
+   * (SUCCESS) 1: Create Event
+   * (FAIL) 2: Create Job based on eventId
+   * (FAIL) 3: Update Event with jobId
+   */
+
+  describe('A failed Event POST request (Job API fails)', () => {
+    before('Clear up databases', (done) => clearUpDatabases(done));
+    it('should fail', done => {
+      supertest(nonTransactionalApp)
+        .post('/events')
+        .send({ publishingDate })
+        .set('monkey_POST_job', 'none/500')
+        .expect(500, done)
+    });
+
+    it('should have created an Event in MongoDB', done => {
+      Event
+        .count()
+        .then(res => {
+          expect(res).to.be.equal(1);
+          done();
+        })
+        .catch(err => {
+          done(err);
+        });
+    });
+
+    it('should not have created a Job in Redis', done => {
+      Job
+        .count()
+        .then(res => {
+          expect(res).to.be.equal(0);
+          done();
+        })
+        .catch(err => {
+          done(err);
+        });
+    });
+  });
+
+  /**
+   * Simulate a common Request to the Wrapper Api of the non transactional system
+   * The Event API should throw an Error on the Update Operation,
+   * while an Event and a Job were already created
+   * (SUCCESS) 1: Create Event
+   * (SUCCESS) 2: Create Job based on eventId
+   * (FAIL) 3: Update Event with jobId
+   */
+
+  describe('A failed Event POST request (Event API fails on Update Operation)', () => {
+    before('Clear up databases', (done) => clearUpDatabases(done));
+    it('should fail', done => {
+      supertest(nonTransactionalApp)
+        .post('/events')
+        .send({ publishingDate })
+        .set('monkey_PUT_event', 'none/500')
+        .expect(500, done)
+    });
+
+    it('should have created an Event in MongoDB', done => {
+      Event
+        .count()
+        .then(res => {
+          expect(res).to.be.equal(1);
+          done();
+        })
+        .catch(err => {
+          done(err);
+        });
+    });
+
+    it('should have created a Job in Redis', done => {
+      Job
+        .count()
+        .then(res => {
+          expect(res).to.be.equal(1);
           done();
         })
         .catch(err => {
